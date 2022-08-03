f05d8e840a8d94d8a6c9c0531e6aa665f4f3465a
samsonajulor <samsonajulor@gmail.com>
2022-08-03 21:15:58 +0100

    added recommended videos

---
+++ src/components/RecommendedVideos.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import VideoCard from './VideoCard'
+
+import './RecommendedVideos.css'
+
+function RecommendedVideos() {
+  return (
+    <div className='recommendedVideos'>
+      <h2>Recommended</h2>
+      <div className='recommendedVideos__videos'>
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='2.3M Views'
+          timestamp='3 days ago'
+          channelImage='https://scontent-cpt1-1.xx.fbcdn.net/v/t1.6435-9/127929844_192909205774620_4852570612652802182_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHzoiPboR1R3cVDHmZA5Bdbw52vDjWhjlTDna8ONaGOVH2G6lM8WdjU-8LVh4MTzwar5p72xiBhQxZhsorFEOnw&_nc_ohc=40QHxjZz3qAAX90Qh8h&_nc_ht=scontent-cpt1-1.xx&oh=00_AT-i2dOk5GKmm2BiKUn42RX4pdfJfGlzSMXrRThcUpP_QA&oe=62190A3F'
+          channel='Samson Ajulor'
+          image='https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2dyYXBoaWNzLWNhcmQtdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fSwidG9Gb3JtYXQiOiJhdmlmIn19'
+        />
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='2.3M Views'
+          timestamp='3 days ago'
+          channelImage='https://scontent-cpt1-1.xx.fbcdn.net/v/t1.6435-9/127929844_192909205774620_4852570612652802182_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHzoiPboR1R3cVDHmZA5Bdbw52vDjWhjlTDna8ONaGOVH2G6lM8WdjU-8LVh4MTzwar5p72xiBhQxZhsorFEOnw&_nc_ohc=40QHxjZz3qAAX90Qh8h&_nc_ht=scontent-cpt1-1.xx&oh=00_AT-i2dOk5GKmm2BiKUn42RX4pdfJfGlzSMXrRThcUpP_QA&oe=62190A3F'
+          channel='Samson Ajulor'
+          image='https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2dyYXBoaWNzLWNhcmQtdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fSwidG9Gb3JtYXQiOiJhdmlmIn19'
+        />
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='2.3M Views'
+          timestamp='3 days ago'
+          channelImage='https://scontent-cpt1-1.xx.fbcdn.net/v/t1.6435-9/127929844_192909205774620_4852570612652802182_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHzoiPboR1R3cVDHmZA5Bdbw52vDjWhjlTDna8ONaGOVH2G6lM8WdjU-8LVh4MTzwar5p72xiBhQxZhsorFEOnw&_nc_ohc=40QHxjZz3qAAX90Qh8h&_nc_ht=scontent-cpt1-1.xx&oh=00_AT-i2dOk5GKmm2BiKUn42RX4pdfJfGlzSMXrRThcUpP_QA&oe=62190A3F'
+          channel='Samson Ajulor'
+          image='https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2dyYXBoaWNzLWNhcmQtdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fSwidG9Gb3JtYXQiOiJhdmlmIn19'
+        />
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='2.3M Views'
+          timestamp='3 days ago'
+          channelImage='https://scontent-cpt1-1.xx.fbcdn.net/v/t1.6435-9/127929844_192909205774620_4852570612652802182_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHzoiPboR1R3cVDHmZA5Bdbw52vDjWhjlTDna8ONaGOVH2G6lM8WdjU-8LVh4MTzwar5p72xiBhQxZhsorFEOnw&_nc_ohc=40QHxjZz3qAAX90Qh8h&_nc_ht=scontent-cpt1-1.xx&oh=00_AT-i2dOk5GKmm2BiKUn42RX4pdfJfGlzSMXrRThcUpP_QA&oe=62190A3F'
+          channel='Samson Ajulor'
+          image='https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2dyYXBoaWNzLWNhcmQtdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fSwidG9Gb3JtYXQiOiJhdmlmIn19'
+        />
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='2.3M Views'
+          timestamp='3 days ago'
+          channelImage='https://scontent-cpt1-1.xx.fbcdn.net/v/t1.6435-9/127929844_192909205774620_4852570612652802182_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHzoiPboR1R3cVDHmZA5Bdbw52vDjWhjlTDna8ONaGOVH2G6lM8WdjU-8LVh4MTzwar5p72xiBhQxZhsorFEOnw&_nc_ohc=40QHxjZz3qAAX90Qh8h&_nc_ht=scontent-cpt1-1.xx&oh=00_AT-i2dOk5GKmm2BiKUn42RX4pdfJfGlzSMXrRThcUpP_QA&oe=62190A3F'
+          channel='Samson Ajulor'
+          image='https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2dyYXBoaWNzLWNhcmQtdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fSwidG9Gb3JtYXQiOiJhdmlmIn19'
+        />
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='2.3M Views'
+          timestamp='3 days ago'
+          channelImage='https://scontent-cpt1-1.xx.fbcdn.net/v/t1.6435-9/127929844_192909205774620_4852570612652802182_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHzoiPboR1R3cVDHmZA5Bdbw52vDjWhjlTDna8ONaGOVH2G6lM8WdjU-8LVh4MTzwar5p72xiBhQxZhsorFEOnw&_nc_ohc=40QHxjZz3qAAX90Qh8h&_nc_ht=scontent-cpt1-1.xx&oh=00_AT-i2dOk5GKmm2BiKUn42RX4pdfJfGlzSMXrRThcUpP_QA&oe=62190A3F'
+          channel='Samson Ajulor'
+          image='https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2dyYXBoaWNzLWNhcmQtdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fSwidG9Gb3JtYXQiOiJhdmlmIn19'
+        />
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='2.3M Views'
+          timestamp='3 days ago'
+          channelImage='https://scontent-cpt1-1.xx.fbcdn.net/v/t1.6435-9/127929844_192909205774620_4852570612652802182_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHzoiPboR1R3cVDHmZA5Bdbw52vDjWhjlTDna8ONaGOVH2G6lM8WdjU-8LVh4MTzwar5p72xiBhQxZhsorFEOnw&_nc_ohc=40QHxjZz3qAAX90Qh8h&_nc_ht=scontent-cpt1-1.xx&oh=00_AT-i2dOk5GKmm2BiKUn42RX4pdfJfGlzSMXrRThcUpP_QA&oe=62190A3F'
+          channel='Samson Ajulor'
+          image='https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2dyYXBoaWNzLWNhcmQtdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fSwidG9Gb3JtYXQiOiJhdmlmIn19'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default RecommendedVideos
